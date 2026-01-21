87c97081ce2c802635552aa1b5a9d827b4a4ee18
MelonHell <MelonHell@ya.ru>
2026-01-21 14:26:51 +0300

    выпил всего лишнего

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,48 +1,21 @@
-import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
+import { Section, Cell, List } from '@telegram-apps/telegram-ui';
 import type { FC } from 'react';
 
-import { Link } from '@/components/Link/Link.tsx';
-import { Page } from '@/components/Page.tsx';
-
-import tonSvg from './ton.svg';
+import { Link } from '@/components/Link/Link';
+import { Page } from '@/components/Page';
 
 export const IndexPage: FC = () => {
   return (
-    <Page back={false}>
-      <List>
-        <Section
-          header="Features"
-          footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
-        >
-          <Link to="/plant-doctor">
-            <Cell subtitle="Диагностика + рекомендации по удобрению">
-              🌿 Доктор Растение
-            </Cell>
-          </Link>
-          <Link to="/ton-connect">
-            <Cell
-              before={<Image src={tonSvg} style={{ backgroundColor: '#007AFF' }}/>}
-              subtitle="Connect your TON wallet"
-            >
-              TON Connect
-            </Cell>
-          </Link>
-        </Section>
-        <Section
-          header="Application Launch Data"
-          footer="These pages help developer to learn more about current launch information"
-        >
-          <Link to="/init-data">
-            <Cell subtitle="User data, chat information, technical data">Init Data</Cell>
-          </Link>
-          <Link to="/launch-params">
-            <Cell subtitle="Platform identifier, Mini Apps version, etc.">Launch Parameters</Cell>
-          </Link>
-          <Link to="/theme-params">
-            <Cell subtitle="Telegram application palette information">Theme Parameters</Cell>
-          </Link>
-        </Section>
-      </List>
-    </Page>
+      <Page back={false}>
+        <List>
+          <Section header="Dr. Plant 🌿" footer="Быстрая диагностика и рекомендации">
+            <Link to="/plant-doctor">
+              <Cell subtitle="Что с растением и чем подкормить">
+                🌱 Диагностика растения
+              </Cell>
+            </Link>
+          </Section>
+        </List>
+      </Page>
   );
 };
